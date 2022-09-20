d1868cbe30f3c9d36489b30fdbf04a2c54de5bb6
Djougabench <deehans4@gmail.com>
2022-09-20 10:34:56 +0200

    new subscriptionId

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,7 +6,7 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
-        subscriptionId: "588",
+        subscriptionId: "1854",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: "100000000000000000", // 0.1 ETH
@@ -17,7 +17,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        subscriptionId:"157",
+        subscriptionId:"1854",
         callbackGasLimit:"500000",
         interval:"30",
 
